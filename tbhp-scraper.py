7463eb120942cede0783319b98bec7040721666b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-01-25 22:11:03 +0530

    simplify code

--- tbhp-scraper.py
+++ tbhp-scraper.py
@@ -10,9 +10,7 @@ def get_soup(link):
     r = requests.get(link)
     if r.status_code != requests.codes.ok:
         return None
-    html_doc = r.text
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    return soup
+    return BeautifulSoup(r.text, 'html.parser')
 
 def main():
     soup = get_soup('https://www.team-bhp.com/forum/official-new-car-reviews/')
@@ -26,14 +24,11 @@ def main():
 
     # get list of all links to be navigated to
     # go to each of those links, and get links to vehicle reviews
-
     for link in soup.find_all('a'):
         link_title = link.get('title')
         if(str(link_title).startswith("Show results")):
-            # print(link_title, link.get('href'))
             navigation_pages.add(link.get('href'))
 
-    counter = 0
     # now go to each page, and get the vehicle review list
     for page in navigation_pages:
         soup = get_soup(page)
@@ -41,8 +36,7 @@ def main():
             for link in soup.find_all('a'):
                 link_id = link.get('id')
                 if str(link_id).startswith('thread'):
-                    counter += 1
-                    print(counter, link.text, link.get('href'))
+                    print(link.text, link.get('href'))
                     review[link.text] = link.get('href')
 
     
