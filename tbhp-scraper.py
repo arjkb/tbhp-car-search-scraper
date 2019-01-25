a4e40fd012214c93ee297145e9e0f4afc279232d
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2019-01-25 22:04:00 +0530

    define function to get a page and return its soup object

--- tbhp-scraper.py
+++ tbhp-scraper.py
@@ -6,12 +6,19 @@ def search(carname, reviews):
         if carname in k.lower():
             print(k, reviews[k])
 
-def main():
-    r = requests.get('https://www.team-bhp.com/forum/official-new-car-reviews/')
-    # TODO: ensure status code is 200
-
+def get_soup(link):
+    r = requests.get(link)
+    if r.status_code != requests.codes.ok:
+        return None
     html_doc = r.text
     soup = BeautifulSoup(html_doc, 'html.parser')
+    return soup
+
+def main():
+    soup = get_soup('https://www.team-bhp.com/forum/official-new-car-reviews/')
+    if soup == None:
+        print("Error fetching page")
+        exit(0)
 
     navigation_pages = set()
     navigation_pages.add('https://www.team-bhp.com/forum/official-new-car-reviews/')
@@ -22,26 +29,21 @@ def main():
 
     for link in soup.find_all('a'):
         link_title = link.get('title')
-
         if(str(link_title).startswith("Show results")):
-            print(link_title, link.get('href'))
+            # print(link_title, link.get('href'))
             navigation_pages.add(link.get('href'))
 
     counter = 0
     # now go to each page, and get the vehicle review list
     for page in navigation_pages:
-        r = requests.get(page)
-        # TODO: ensure status code is 200
-        html_doc = r.text
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        
-        for link in soup.find_all('a'):
-            link_id = link.get('id')
-
-            if str(link_id).startswith('thread'):
-                counter += 1
-                print(counter, link.text, link.get('href'))
-                review[link.text] = link.get('href')
+        soup = get_soup(page)
+        if soup != None:
+            for link in soup.find_all('a'):
+                link_id = link.get('id')
+                if str(link_id).startswith('thread'):
+                    counter += 1
+                    print(counter, link.text, link.get('href'))
+                    review[link.text] = link.get('href')
 
     
     search("mercedes", review)
